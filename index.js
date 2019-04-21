559a7db8c4f8df79f80ddebe567ad908092699d8
Shahriar Nooh Kabir <47024922+scotchontherocks31@users.noreply.github.com>
2019-04-21 16:06:06 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Index login file
 //discord.js module reqired
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
 //Create a new Discord client
 const client = new Discord.Client();
@@ -8,9 +9,16 @@ const client = new Discord.Client();
 //When client is ready, run the code
 //This event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Ready.');
 });
 
+client.on('message', message => {
+	//console.log(message.content);//For messages sent to the console
+	if(message.content.startsWith(`${prefix}hello`)){
+		message.channel.send("Hi!");
+	}
+})
+
+client.login(token);
+
 //Login to Discord with bot token
-const config = require('./config.json');
-//client.login('NTY5NDI3NzQ2MzgwNTc4ODI2.XLw4mQ.DtnrXROz0GbKH5VQ_06XdezVYjI');
